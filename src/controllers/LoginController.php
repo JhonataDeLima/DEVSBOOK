7379a2ffda79c04312c55009ba318daa7f924fe8
JhonadaDeLima <jhonatapereira28@gmail.com>
2024-01-15 23:29:28 -0300

    start project devsbook

---
+++ src/controllers/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+namespace src\controllers;
+
+use \core\Controller;
+
+class LoginController extends Controller {
+
+    private $loggedUser;
+
+    public function signin(){
+        echo 'login';
+    }
+
+    public function signup() {
+
+        echo 'cadastro';
+    }
+
+
+
+}
